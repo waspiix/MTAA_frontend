eb17c7d0220b57a7dc3acbce6ec05d01bacafdcb
waspiix <t.horan8777@gmail.com>
2025-05-13 14:44:25 +0200

    small update

--- screens/BuyTicketScreen.js
+++ screens/BuyTicketScreen.js
@@ -31,7 +31,7 @@ export default function BuyTicketScreen({ route, navigation }) {
 
   // Funkcia na získanie obrázku vlaku podľa mena
   const getTrainImage = (trainName) => {
-    console.log(train.name);
+
     if (!trainName) return require('../assets/trains/default.jpg');
 
     const typeMatch = trainName.match(/^[A-Za-z]{2}/);
